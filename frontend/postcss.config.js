07309a5df1526ff4fcd586e7dc3e0956bc2982a9
damSPBR <damspbr@gmail.com>
2026-04-26 19:01:41 -0300

    Fase de Integração concluida com Sucesso

---
+++ frontend/postcss.config.js
@@ -0,0 +1,8 @@
+export default {
+  plugins: {
+    // Agora usamos o novo pacote que acabamos de instalar
+    "@tailwindcss/postcss": {}, 
+    // Mantemos o autoprefixer para compatibilidade entre navegadores
+    autoprefixer: {},
+  },
+}
